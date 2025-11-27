52511771ea8d7432fff4dbaf8f476a5ed85537ea
kudith <ginanjar.aditya15@gmail.com>
2025-11-27 19:57:28 +0700

    fix: Update import statements in vite.config.js to use node module paths

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
-import path from "path";
-import { fileURLToPath } from "url";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/foundation-transparency-platform/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
