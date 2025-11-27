44be2db518b9263bffe16b0b890f07b7380cb4a7
kudith <ginanjar.aditya15@gmail.com>
2025-11-27 20:24:06 +0700

    refactor: Remove vite-plugin-static-copy and related configurations from vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,26 +3,13 @@ import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
-import { viteStaticCopy } from "vite-plugin-static-copy";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/foundation-transparency-platform/",
-  plugins: [
-    react(),
-    tailwindcss(),
-    viteStaticCopy({
-      targets: [
-        {
-          src: "public/_redirects",
-          dest: ".",
-        },
-      ],
-    }),
-  ],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
