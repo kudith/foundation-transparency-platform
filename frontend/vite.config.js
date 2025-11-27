cd9e6ca58be3b775dabbfc72b08d64a150c55cce
kudith <ginanjar.aditya15@gmail.com>
2025-11-27 20:05:39 +0700

    fix: Update base path in vite.config.js for correct deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,7 +9,7 @@ const __dirname = path.dirname(__filename);
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/foundation-transparency-platform/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
